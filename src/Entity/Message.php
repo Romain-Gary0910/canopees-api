512961716d6300c5a436f526cf80713212e92dc4
Romain-Gary0910 <romain.gary0910@gmail.com>
2025-10-17 16:37:39 +0200

    Modification de Message.php pour gérer les devis

--- src/Entity/Message.php
+++ src/Entity/Message.php
@@ -2,37 +2,80 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Delete;
 use App\Repository\MessageRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use ApiPlatform\Metadata\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: MessageRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    operations: [
+        new GetCollection(),
+        new Post(),
+        new Get(),
+        new Put(),
+        new Patch(),
+        new Delete(),
+    ],
+    normalizationContext: ['groups' => ['message:read']],
+    denormalizationContext: ['groups' => ['message:write']]
+)]
 class Message
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['message:read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 100)]
+    #[Groups(['message:read', 'message:write'])]
     private ?string $nom = null;
 
     #[ORM\Column(length: 100)]
+    #[Groups(['message:read', 'message:write'])]
     private ?string $prenom = null;
 
     #[ORM\Column(length: 150)]
+    #[Groups(['message:read', 'message:write'])]
     private ?string $email = null;
 
     #[ORM\Column(length: 20)]
+    #[Groups(['message:read', 'message:write'])]
     private ?string $telephone = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['message:read', 'message:write'])]
     private ?string $objet = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Groups(['message:read', 'message:write'])]
     private ?string $message = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(['message:read', 'message:write'])]
+    private ?string $reponse = null;
+
+    #[ORM\Column(type: Types::BOOLEAN)]
+    #[Groups(['message:read', 'message:write'])]
+    private bool $traite = false;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[Groups(['message:read', 'message:write'])]
+    private \DateTimeInterface $createdAt;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+        $this->traite = false;
+    }
 
     public function getId(): ?int
     {
@@ -108,6 +151,39 @@ class Message
     {
         $this->message = $message;
 
+        return $this;
+    }
+
+    public function getReponse(): ?string
+    {
+        return $this->reponse;
+    }
+
+    public function setReponse(?string $reponse): static
+    {
+        $this->reponse = $reponse;
+        return $this;
+    }
+
+    public function isTraite(): bool
+    {
+        return $this->traite;
+    }
+
+    public function setTraite(bool $traite): static
+    {
+        $this->traite = $traite;
+        return $this;
+    }
+
+    public function getCreatedAt(): \DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): static
+    {
+        $this->createdAt = $createdAt;
         return $this;
     }
 }
